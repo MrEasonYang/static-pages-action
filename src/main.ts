cea876af70db2221880cce855db5df5147bfd55a
Eason Yang <me@easonyang.com>
2021-08-03 23:21:06 +0800

    Init static website action.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,56 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import axios from 'axios'
+import xmlParser from 'xml-js'
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
+  if (github.context.eventName !== 'push') {
+    core.info('Not a push event, skip the action.')
+    return
   }
+
+  const beforeCommit = github.context.payload.before
+  const afterCommit = github.context.payload.after
+  core.info(`HEAD^ commit: ${beforeCommit}`)
+  core.info(`HEAD commit: ${afterCommit}`)
+
+  if (!beforeCommit || !afterCommit) {
+    core.setFailed('Invalid commits info, failed to process the action.')
+    return
+  }
+
+  const octokit = github.getOctokit(core.getInput('token', {required: true}))
+
+  const response = await octokit.rest.repos.compareCommitsWithBasehead({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    basehead: afterCommit
+  })
+
+  if (response.status !== 200) {
+    core.setFailed('Failed to request GitHub API to compare commits.')
+    return
+  }
+  if (response.data.files === undefined) {
+    core.setFailed('No files fount in the head commits.')
+    return
+  }
+
+  const filenames = {} as string[]
+  for (const file of response.data.files) {
+    if (file.status === 'added') {
+      filenames.push(file.filename)
+    }
+  }
+
+  const rssUrl = core.getInput('rss_url', {required: true})
+  const resp = await axios.get(rssUrl)
+  const rssData = JSON.parse(
+    xmlParser.xml2json(resp.data, {compact: true, spaces: 2})
+  )
+
+  core.setOutput('filenames', filenames)
+  core.setOutput('rssData', rssData)
 }
 
 run()
